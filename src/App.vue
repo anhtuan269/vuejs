f5b6c128d36721da6fb65a38be1668423747045b
zeroo <anhtuan090795@gmail.com>
2021-02-22 23:08:58 +0700

    nhap mon vuejs

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,63 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu />
+    <input type = "text" v-model="name"> 
+    <div> 
+      {{name}}
+    </div>
+    <div v-show="isShow"> 
+      <h2> Lap trinh vien tap su </h2>
+    </div>
+    <button @click="isShow = ! isShow"> An / Hien</button>
+    <div id = "conca">
+      <h1 :id="id" :class = "{dacho: selected}"> TML Tuan ngu nhu cho</h1>
+      <input type = "text" :disabled="inputDisabled" v-model="nema"> 
+      <button @click="inputDisabled = !inputDisabled" > mo / khoa </button>
+      <button id="click" @click="selected = !selected"> click  </button>
+
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/Menu.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      name: 'Nguyen Tat Anh Tuan',
+      isShow: true,
+      id: 'dachon',
+      inputDisabled: true,
+      selected: true,
+      nema: 'Nguyen Tat Anh Tuan'
+
+    
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    Menu
   }
 }
 </script>
 
 <style>
+.dacho {
+  color: red  !important;
+}
+
+#click {
+ max-width: 1200px;
+  margin: 50px auto ;
+  display: block;
+}
+#conca {
+  margin-bottom: 300px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1> ĐÂY là Menu</h1>
+    </div>
+    
+</template>
+<script>
+export default {
+    Props: {
+
+    }
+}
+</script>
+<style scoped>
+
+</style>
